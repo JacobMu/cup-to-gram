028d989f0c0d488a501373a2bd622596f4cbd6d9
Jakub Murin <me@jakubmurin.dev>
2026-05-04 17:39:00 +0200

    refactor: improve readability by adding braces to conditional statement

--- src/features/dom-traversal/injectLabel.ts
+++ src/features/dom-traversal/injectLabel.ts
@@ -1,6 +1,8 @@
 export function injectLabel(textNode: Text, insertAfterOffset: number, label: string): void {
   const parent = textNode.parentNode;
-  if (!parent) return;
+  if (!parent) {
+    return;
+  }
 
   const after = textNode.splitText(insertAfterOffset);
   const span = document.createElement("span");

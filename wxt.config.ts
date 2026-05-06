4176cebc122b984f74e8ae02552d96e91cbac489
Jakub Murin <me@jakubmurin.dev>
2026-05-06 17:49:43 +0200

    feat: update favicon configuration and add new icon assets

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from "wxt";
 
 export default defineConfig({
-	srcDir: "src",
-	manifest: {
-		icons: {
-			32: "assets/icon-32.png",
-		},
-		name: "Cup to Gram",
-		description:
-			"Browser extension that converts cup measurements to grams on recipe websites",
-	},
+  srcDir: "src",
+  manifest: {
+    permissions: ["favicon"],
+    name: "Cup to Gram",
+    description: "Browser extension that converts cup measurements to grams on recipe websites",
+  },
 });

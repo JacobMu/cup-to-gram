0e082f506ccba56f53acd7cd43310759fd64c322
Jakub Murin <me@jakubmurin.dev>
2026-05-06 17:22:08 +0200

    feat: add SVG icons for different sizes and update configuration to include them

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from "wxt";
 export default defineConfig({
 	srcDir: "src",
 	manifest: {
+		icons: {
+			16: "assets/icon-16.png",
+			32: "assets/icon-32.png",
+			48: "assets/icon-48.png",
+		},
 		name: "Cup to Gram",
 		description:
 			"Browser extension that converts cup measurements to grams on recipe websites",

b74c3e87774947cd9b87428a069c113a19523166
Jakub Murin <me@jakubmurin.dev>
2026-05-06 17:27:58 +0200

    feat: add SVG icons for different sizes and update configuration to i… (#1)
    * feat: add SVG icons for different sizes and update configuration to include them
    
    * feat: remove unused favicon sizes and update configuration

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "wxt";
 export default defineConfig({
 	srcDir: "src",
 	manifest: {
+		icons: {
+			32: "assets/icon-32.png",
+		},
 		name: "Cup to Gram",
 		description:
 			"Browser extension that converts cup measurements to grams on recipe websites",

057a3cd66490bb9d3f4cc334b2393a2bb5da9219
Jakub Murin <me@jakubmurin.dev>
2026-05-06 17:24:52 +0200

    feat: remove unused favicon sizes and update configuration

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,9 +4,7 @@ export default defineConfig({
 	srcDir: "src",
 	manifest: {
 		icons: {
-			16: "assets/icon-16.png",
 			32: "assets/icon-32.png",
-			48: "assets/icon-48.png",
 		},
 		name: "Cup to Gram",
 		description:
